a595978d9270338ad910cd33bf363c15eca1812a
alonardo <105623292+alonardo@users.noreply.github.com>
2022-08-15 22:32:08 -0600

    day 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import SimpleBottomNavigation from './components/SimpleBottomNavigation';
+import MyBook from './components/MyBooks';
+
+function App() {
+  return (
+    <>
+    <NavBar>
+    </NavBar>
+    <MyBook/>
+    <SimpleBottomNavigation>  
+    </SimpleBottomNavigation>
+    </>
+  );
+}
+
+export default App;
